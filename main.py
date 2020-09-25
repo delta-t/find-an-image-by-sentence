7028632da7a89da2a349824e1ddb4b8b97facb29
newman001 <andrey.fedyashev@gmail.com>
2020-09-25 14:26:40 +0500

    Added slideshow of 5 images

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
 import json
-import cv2
-import re
 import traceback
-import numpy as np
 from flask import Flask, render_template, redirect, url_for, request, flash, jsonify, Response, abort
 from PIL import ImageFont, ImageDraw, Image
 from markupsafe import escape
@@ -18,7 +15,8 @@ from PIL import Image
 
 
 # Flickr api access key 
-
+api_key = '6d9ae033d1c204dbdb9c1b46404589d5'
+api_password = '147314892eaa2340'
 flickr=flickrapi.FlickrAPI(api_key, api_password, cache=True)
 
 app = Flask(__name__)
@@ -26,6 +24,7 @@ app.config.update(dict(
     SECRET_KEY="123",
     WTF_CSRF_SECRET_KEY="122"
 ))
+
 UPLOAD_FOLDER = 'static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -35,16 +34,13 @@ term_extractor = TermExtractor()
 
 
 def get_images(keyword):
-    # keyword = 'siberian husky'
-    # keywords = str(keywords).strip().split(',')
-    # for keyword in keywords:
     print(keyword)
     photos = flickr.walk(
                         text=keyword,
                         tag_mode='all',
                         tags=keyword,
                         extras='url_c',
-                        per_page=50,           # may be you can try different numbers..
+                        per_page=50,
                         sort='relevance')
 
     urls = []
@@ -56,44 +52,49 @@ def get_images(keyword):
             correct_urls += 1
         if correct_urls > 5:
             break
-    print (urls)
+    print(urls)
     # Download image from the url and save it
     for i, url in enumerate(urls):
         urllib.request.urlretrieve(url, f'./static/{keyword}_{i}.jpg')
         # Resize the image and overwrite it
-        image = Image.open(f'./static/{keyword}_{i}.jpg') 
+        image = Image.open(f'./static/{keyword}_{i}.jpg')
         image = image.resize((1920, 1080), Image.ANTIALIAS)
         draw_description(image, keyword)
         image.save(f'./static/{keyword}_{i}.jpg')
 
 def draw_description(img_pil, description):
-    # img = cv2.imread('./static/setosa.jpg')
-    # print(img.shape)
-    # img = cv2.resize(img, (1920, 1080))
 
     b,g,r,a = 0,255,0,0
     fontpath = "./fonts/Montserrat.ttf"
-    
+
     font = ImageFont.truetype(fontpath, 32)
-    # img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil)
     draw.text((50, 100),  description, font = font, fill = (b, g, r, a))
 
 
 class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    input_data = StringField(u'Введите слово или предложение:', validators=[DataRequired()])
 
 
 @app.route('/', methods=('GET', 'POST'))
 def greetings_page():
+
     form = MyForm()
     if form.validate_on_submit():
-        # get_images(form.name.data)
-        print(form.name)
-        text = form.name.data
+        print(form.input_data)
+        text = form.input_data.data
         for term in term_extractor(text, nested=True):
             get_images(term.normalized)
             print(term.normalized, term.count)
-        return render_template('index.html', form=form, user_image='static/setosa.jpg')
-    return render_template('index.html', form=form)
+        
+        show_images = [f for f in os.listdir('./static/') if os.path.isfile(os.path.join('./static/', f))]
 
+        return render_template('index.html', form=form, first_image=f'static/{show_images[0]}',
+                                                        second_image=f'static/{show_images[1]}',
+                                                        third_image=f'static/{show_images[2]}',
+                                                        fourth_image=f'static/{show_images[3]}',
+                                                        fifth_image=f'static/{show_images[4]}')
+    for img in os.listdir('./static/'):
+        if os.path.isfile(os.path.join('./static/', img)):
+            os.remove(os.path.join('./static/', img))
+    return render_template('index.html', form=form)
