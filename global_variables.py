da680b48887f257f43b3b05e2a7b57bc4f0b157a
newman001 <andrey.fedyashev@gmail.com>
2020-09-27 17:04:49 +0500

    refactoring dockerfile and main script

---
+++ global_variables.py
@@ -0,0 +1,12 @@
+# Flickr api access key 
+API_KEY = '6d9ae033d1c204dbdb9c1b46404589d5'
+API_PASSWORD = '147314892eaa2340'
+
+APP_SECRET_KEY = '123'
+APP_WTF_CSRF_SECRET_KEY = '122'
+
+UPLOAD_FOLDER = 'static/images/'
+DATABASE_URI = 'sqlite:////tmp/test.db'
+
+FONTPATH = './fonts/Montserrat_bold.ttf'
+B,G,R,A = 0,255,0,0
--- main.py
+++ main.py
@@ -13,27 +13,26 @@ import flickrapi
 import urllib
 from PIL import Image
 from flask_sqlalchemy import SQLAlchemy
+from global_variables import *
 
 
-
-# Flickr api access key 
-api_key = '6d9ae033d1c204dbdb9c1b46404589d5'
-api_password = '147314892eaa2340'
-flickr=flickrapi.FlickrAPI(api_key, api_password, cache=False)
-
+# configure flask application
 app = Flask(__name__)
 app.config.update(dict(
-    SECRET_KEY="123",
-    WTF_CSRF_SECRET_KEY="122"
+    SECRET_KEY=APP_SECRET_KEY,
+    WTF_CSRF_SECRET_KEY=APP_WTF_CSRF_SECRET_KEY
 ))
-
-UPLOAD_FOLDER = 'static/images/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-app.config['CACHE_TYPE'] = 'null'
 
+# clear image folder before starting
+for img in os.listdir(app.config['UPLOAD_FOLDER']):
+    if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], img)):
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], img))
+
+# database initialization
 db = SQLAlchemy(app)
 
 class SentencesAndKeywords(db.Model):
@@ -46,15 +45,35 @@ class SentencesAndKeywords(db.Model):
         self.sentence = sentence
         self.keywords = keywords
 
+
+# flikr api initialization
+flickr=flickrapi.FlickrAPI(API_KEY, API_PASSWORD, cache=True)
+
 # Keywords extractor
 term_extractor = TermExtractor()
-fontpath = "./fonts/Montserrat_bold.ttf"
-font = ImageFont.truetype(fontpath, 64)
-b,g,r,a = 0,255,0,0
 
+# font setting up
+font = ImageFont.truetype(FONTPATH, 64)
+
+
+def insert_to_db(full_text: str, terms: str) -> None:
+    '''
+        full_text - string variable, entered text
+        terms - string variable, keywords
+        Insert full_text and keywords in database
+    '''
+    found_sentence = SentencesAndKeywords.query.filter_by(sentence=full_text).all()
+    if not found_sentence:
+        add_data = SentencesAndKeywords(full_text, ", ".join(terms))
+        db.session.add(add_data)
+        db.session.commit()
 
 def get_images(keyword: str, full_text: str) -> None:
-    print(keyword)
+    '''
+        keyword - sting variable
+        full_text - string variable
+        Download images by keyword and drawing full_text on its
+    '''
     photos = flickr.walk(
                         text=keyword,
                         tag_mode='all',
@@ -70,48 +89,42 @@ def get_images(keyword: str, full_text: str) -> None:
         if url is not None:
             urls.append(url)
             correct_urls += 1
-        if correct_urls > 5:
+        if correct_urls > 5: # six images are enough
             break
 
     # Download image from the url and save it
     for i, url in enumerate(urls):
-        urllib.request.urlretrieve(url, f'./static/images/{keyword}_{i}.jpg')
-        # Resize the image and overwrite it
-        image = Image.open(f'./static/images/{keyword}_{i}.jpg')
+        urllib.request.urlretrieve(url, os.path.join('./', f'static/images/{keyword}_{i}.jpg'))
+        # Resize the image and insert the full text, then overwrite it
+        image = Image.open(os.path.join('./', f'static/images/{keyword}_{i}.jpg'))
         image = image.resize((1920, 1080), Image.ANTIALIAS).convert('RGB')
         draw_description(image, full_text)
-        image.save(f'./static/images/{keyword}_{i}.jpg')
+        image.save(os.path.join('./', f'static/images/{keyword}_{i}.jpg'))
 
 def draw_description(img_pil, description: str) -> None:
     draw = ImageDraw.Draw(img_pil)
-    draw.text((20, 1000),  description, font = font, fill = (b, g, r, a))
+    draw.text((20, 1000),  description, font = font, fill = (B, G, R, A))
 
 
 class MyForm(FlaskForm):
     input_data = StringField(u'Введите слово или предложение:', validators=[DataRequired()])
 
+
 @app.before_first_request
 def create_table():
+    '''
+        before first request we must create a database
+    '''
     db.create_all()
 
-@app.before_request
-def clear_upload_folder():
-    for img in os.listdir(app.config['UPLOAD_FOLDER']):
-        if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], img)):
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], img))
-
-def insert_to_db(text, terms):
-    found_sentence = SentencesAndKeywords.query.filter_by(sentence=text).all()
-    if not found_sentence:
-        add_data = SentencesAndKeywords(text, ", ".join(terms))
-        db.session.add(add_data)
-        db.session.commit()
 
 @app.route('/', methods=('GET', 'POST'))
 def greetings_page():
+    '''
+        The main page for input sentences
+    '''
     form = MyForm()
     if form.validate_on_submit():
-        print(form.input_data)
         text = form.input_data.data
         terms = []
         for term in term_extractor(text, nested=True):
@@ -122,9 +135,11 @@ def greetings_page():
 
         show = ['images/' + f for f in os.listdir('./static/images') if os.path.isfile(os.path.join('./static/images', f))]
         return render_template('index.html', form=form, list_images=show)
-
     return render_template('index.html', form=form)
 
 @app.route('/view')
 def view():
+    '''
+        Show database of entered sentences and their keywords
+    '''
     return render_template('view.html', values=SentencesAndKeywords.query.all())
